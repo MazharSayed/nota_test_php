36a12e6f06bd12b95616d811fe224328ed14925d
Sayed Mazhar <31034876+MazharSayed@users.noreply.github.com>
2024-07-17 13:31:03 +0530

    Update task1.php

--- task1.php
+++ task1.php
@@ -1,2 +1,26 @@
-<?
-// Your code is here:
+<?php
+
+$directory = __DIR__ . '/datafiles';
+$fileNames = [];
+
+if (is_dir($directory)) {
+    if ($handle = opendir($directory)) {
+        while (false !== ($entry = readdir($handle))) {
+            if (preg_match('/^[a-zA-Z0-9]+\.ixt$/', $entry)) {
+                $fileNames[] = $entry;
+            }
+        }
+        closedir($handle);
+    } else {
+        echo "Failed to open the directory.";
+    }
+} else {
+    echo "Directory does not exist.";
+}
+
+sort($fileNames);
+
+foreach ($fileNames as $fileName) {
+    echo $fileName . PHP_EOL;
+}
+?>
